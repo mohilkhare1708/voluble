6fa63a84d88ba955797679bd586f4efb17591fcd
mohilkhare1708 <mohilkhare17@gmail.com>
2021-09-21 15:15:41 +0530

    prod fix

--- voluble/settings.py
+++ voluble/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'hx!=n9#0$va@xr$n6a34aew7-mn-p1!khhoi$9&#c^nfcbtk@-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
